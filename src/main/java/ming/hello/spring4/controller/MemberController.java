8ec6a4019ceb510ff1891398ddbd9d07d1aa779c
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-06-29 15:18:08 +0900

    MemberController loginok 구현

--- src/main/java/ming/hello/spring4/controller/MemberController.java
+++ src/main/java/ming/hello/spring4/controller/MemberController.java
@@ -31,8 +31,7 @@ public class MemberController {
     public String joinok(Member m) {
 
         logger.info("member/joinok 호출!");
-        String viewName = "/member/fail";
-
+        String viewName = "redirect:/member/fail";
 
         if (msrv.saveMember(m))
             viewName = "redirect:/member/login"; // 회원가입 처리
@@ -49,11 +48,14 @@ public class MemberController {
     }
 
     @RequestMapping(value = "/member/login", method = RequestMethod.POST)
-    public String loginok(Model m) {
+    public String loginok(Member m) {
+        String viewName = "redirect:/member/loginfail";
 
         logger.info("member/loginok 호출!");
+        if(msrv.loginMember(m))
+            viewName = "redirect:/member/myinfo";
 
-        return "redirect:/member/myinfo";
+        return viewName;
     }
 
     @RequestMapping("/member/myinfo")
