4fa0e4b27be56ce97f1fc61c8d2def44d2391e01
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-06-29 16:50:52 +0900

    MemberService readOneMember 단위 테스트

--- src/test/java/service/MemberServiceUnitTest.java
+++ src/test/java/service/MemberServiceUnitTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -44,4 +45,13 @@ public class MemberServiceUnitTest {
         // assertEquals(테스트메서드, 검증값)
         assertEquals(msrv.loginMember(m), true);
     }
+
+    @Test
+    public void readOneMember() throws Exception {
+       String userid = "abc123";
+
+        // assertEquals(테스트메서드, 검증값)
+        assertNotNull(msrv.readOneMember(userid));
+    }
+
 }
