d682302b48d6e7497b0a3f3a00675f480c1189f3
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-06-30 16:53:25 +0900

    BoardService : readBoard 구현

---
+++ src/main/java/ming/hello/spring4/service/BoardService.java
@@ -0,0 +1,11 @@
+package ming.hello.spring4.service;
+
+import ming.hello.spring4.model.Board;
+
+import java.util.List;
+
+public interface BoardService {
+
+    List<Board> readBoard(int cpg);
+
+}
