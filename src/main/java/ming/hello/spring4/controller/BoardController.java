3cc09634d560a95a44d3b4955aa0690df2260add
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-06-30 16:54:51 +0900

    BoardController : list 구현

--- src/main/java/ming/hello/spring4/controller/BoardController.java
+++ src/main/java/ming/hello/spring4/controller/BoardController.java
@@ -1,7 +1,9 @@
 package ming.hello.spring4.controller;
 
+import ming.hello.spring4.service.BoardService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+
+    @Autowired BoardService bsrv;
+
     private Logger logger = LogManager.getLogger(BoardController.class);
 
     @GetMapping("/list")
-    public String list(Model m) {
+    public String list(Model m, int cpg) {
+        logger.info("board/list 호출!!");
+
+        m.addAttribute("boards", bsrv.readBoard(cpg));
 
         return "board/list.tiles";
     }
