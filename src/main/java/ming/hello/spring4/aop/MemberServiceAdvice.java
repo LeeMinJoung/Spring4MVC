6352f49dcaeb167b1ed8d31486f501e75a30e901
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-03 16:59:29 +0900

    join에 대해 포인트컷, 어드바이스 추가

--- src/main/java/ming/hello/spring4/aop/MemberServiceAdvice.java
+++ src/main/java/ming/hello/spring4/aop/MemberServiceAdvice.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpSession;
@@ -16,7 +17,13 @@ public class MemberServiceAdvice {
 
     private Logger logger = LogManager.getLogger(MemberServiceAdvice.class);
 
-    @Around("execution(* ming.hello.spring4.controller.MemberController.myinfo(..))")
+    @Pointcut("execution(* ming.hello.spring4.controller.MemberController.myinfo(..))")
+    public void myinfoPoint() {}
+
+    @Pointcut("execution(* ming.hello.spring4.controller.MemberController.join(..))")
+    public void joinPoint() {}
+
+    @Around("myinfoPoint()")
     public Object myinfoAOPProcess(ProceedingJoinPoint pjp)
             throws Throwable {
         logger.info("myinfoAOPProcess 호출!!");
@@ -32,10 +39,29 @@ public class MemberServiceAdvice {
         if(sess.getAttribute("member") == null)
             return "redirect:/member/login";
 
-
         //
         Object obj = pjp.proceed();
         return obj;
     }
 
+    @Around("joinPoint()")
+    public Object joinAOPProcess(ProceedingJoinPoint pjp)
+            throws Throwable {
+        logger.info("joinAOPProcess 호출!!");
+        HttpSession sess = null;
+
+        for (Object o : pjp.getArgs()) {
+            if (o instanceof HttpSession)
+                sess = (HttpSession) o;
+        }
+
+        // 포인트컷 대상 메서드 실행
+        // 로그인을 안했다면 -> join으로 이동
+        // 로그인을 했다면 -> myinfo로 이동
+        if(sess.getAttribute("member") != null)
+            return "redirect:/member/myinfo";
+
+        Object obj = pjp.proceed();
+        return obj;
+    }
 }
